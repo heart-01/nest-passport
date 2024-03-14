ad51b2d1915188dc67a9d01d0cded70ff66c65e3
siwat jermwatthana <siwat.j@hiveground.com>
2024-03-14 19:36:41 +0700

    Feat: Add service api auth and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
